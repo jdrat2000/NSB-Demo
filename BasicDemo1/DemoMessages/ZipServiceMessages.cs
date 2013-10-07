45a9b99abe6c1bc337754d029369bcb35fe98821
Joe R <fake@server.net>
2013-10-06 23:39:06 -0500

    updated for 4.05

---
+++ BasicDemo1/DemoMessages/ZipServiceMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace DemoMessages
+{
+    public class ZipLookupRequest:IMessage
+    {
+            public string zipcode { get; set; }
+            public Guid UniqueId { get; set; }
+            
+
+       
+    }
+
+    public class ZipLookupResponse : IMessage
+    {
+        public string zipcode { get; set; }
+        public Guid UniqueId { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+     }
+
+}
