3d4b3a210ab93d1703a67a8bcfb5f23aa2185921
Joe R <fake@server.net>
2013-10-06 23:36:34 -0500

    test

---
+++ BasicDemo1/DemoMessages/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace DemoMessages
+{
+ [TimeToBeReceived("00:03:00")] 
+    public class SimpleOrder:IMessage
+    {
+        public long OrderId { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public Guid UniqueId { get; set; }
+
+    }
+}
